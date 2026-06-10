be66185f220e36e6d9a3ee08307f1b2d257e64e5
Rodrigo Mendes <160135146+RodrigoMendes05@users.noreply.github.com>
2026-06-10 17:03:28 +0100

    Update outputs.tf

--- infrastructure/terraform/modules/compute/outputs.tf
+++ infrastructure/terraform/modules/compute/outputs.tf
@@ -9,3 +9,8 @@ output "public_ip" {
 output "private_ip" {
   value = aws_instance.service.private_ip
 }
+
+output "instance_role_arn" {
+  description = "ARN da IAM role associada ao EC2 (usado para queue policy)"
+  value       = aws_iam_role.ssm.arn
+}
